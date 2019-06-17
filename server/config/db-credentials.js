420ea7fd6a421510f7a271a0da6aaa08977a517f
Paul Kogan <paulkog@gmail.com>
2019-06-17 10:32:48 -0400

    setting up environments

--- server/config/db-credentials.js
+++ server/config/db-credentials.js
@@ -1,25 +1,28 @@
 require('dotenv').config();
 
-const host = process.env.PGHOST;
-let database = process.env.PGDATABASE;
-const user = process.env.PGUSER;
-const password = process.env.PGPASSWORD;
-const port = process.env.PGPORT || 5432;
+const env = process.env.NODE_ENV;
+const version = process.env.VERSION;
 
-if (process.env.NODE_ENV === 'production') {
-  database = 'houndDB';
+console.log('NODE_ENV is: ',env)
+
+let host = process.env.DEV_HOST;
+let database = process.env.DEV_DATABASE;
+let user = process.env.DEV_USER;
+let password = process.env.DEV_PASSWORD;
+let port = process.env.DEV_PORT || 5432;
+
+
+if (env === 'production') {
+
+  host = process.env.PROD_HOST;
+  database = process.env.PROD_DATABASE;
+  user = process.env.PROD_USER;
+  password = process.env.PROD_PASSWORD;
+  port = process.env.PROD_PORT || 5432;
 }
 
-if (process.env.NODE_ENV === 'development') {
-  database = 'houndDB';
-}
 
-if (process.env.NODE_ENV === 'test') {
-  database = 'houndDB';
-} else {
-  database = 'houndDB';
-}
 
 module.exports = {
-  host, database, user, password, port,
+  host, database, user, password, port, env, version
 };
