d404bb25d69a5ab79c4559c84333548bab39aebd
paulkogan <paulkog@gmail.com>
2020-01-04 19:28:30 -0500

    review list page showing list

---
+++ client/src/scenes/Energizers/components/Upload/ReviewList.jsx
@@ -0,0 +1,158 @@
+import React, { useState } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import Button from '@material-ui/core/Button';
+import * as cx from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+
+const ReviewList = props => {
+    const {classes, rowsList, columnMap, onClose, sendList} = props;
+    //const [currentList, updateList] = useState(rawList)
+
+    const renderEnergizerRow = (enzRow) => {
+
+        return (
+
+            <tr key={enzRow.index}>
+                {columnMap.map((header, index)=> {
+                    return <td key={index}>{enzRow[header.toString()]}</td>
+                })}
+            </tr>
+        )
+    };
+
+
+    return (
+        <Dialog open fullWidth onClose={ onClose } maxWidth={ 'lg' }>
+            <DialogTitle>         
+                    Review List
+            </DialogTitle>
+
+            <DialogContent >
+
+                    <div className={classes.tableContainer}>
+                        <table className={classes.reviewTable}>
+                            <tbody>
+                               <tr>
+                               {columnMap.map((header, index)=>{
+                                     return <td key={index}>{header}</td>
+                                })}
+                               </tr>
+                               {rowsList.map(enzRow =>{
+                                     return renderEnergizerRow(enzRow)
+                                })}
+                            
+                            </tbody>
+                        </table>
+                    </div>
+
+
+            </DialogContent>
+
+            <DialogActions>
+                <Button color="primary" variant="contained" onClick={onClose}>Cancel</Button>
+                <Button color="primary" variant="contained" onClick={sendList}>Accept List</Button>
+            </DialogActions>
+        </Dialog>
+      );
+
+}
+
+const styles = () => ({
+    root: {
+      padding: '24px 30px',
+    },
+    tableContainer: {
+      width: "90%",
+      textAlign: "left",
+      padding: '10px',
+      border: '2px solid red',
+    },
+    reviewTable: {
+        width: "100%",
+        textAlign: "left",
+        padding: '10px',
+        border: '2px solid blue',
+      },
+
+    title: {
+      fontWeight: 'normal',
+      fontSize: '24px',
+      lineHeight: '19px',
+      color: '#1C1C1C',
+      paddingBottom: '0px',
+    },
+    energizerTitle: {
+      fontSize: '16px',
+      lineHeight: '20px',
+      fontWeight: 'normal',
+      paddingRight: '5px',
+      margin: "0px"
+    },
+    energizerSubTitle: {
+      fontSize: '16px',
+      lineHeight: '20px',
+      fontWeight: 'normal',
+      color: '#494949',
+      textTransform: 'capitalize',
+      margin: "0px"
+    },
+    panelDetails: {
+      borderTop: '1px solid rgba(96,106,116,0.4)',
+      padding: '0px 0',
+      textAlign: "left",
+    },
+    panelDetailsSection: {
+      paddingTop: '5px',
+      paddingBottom: '5px',
+      paddingLeft: '60px',
+      textAlign: "left",
+    },
+    panelDetailsSectionTitle: {
+      fontSize: '12px',
+      lineHeight: '14px',
+      textTransform: 'uppercase',
+      color: '#606A74',
+      fontWeight: 'normal',
+      margin: "0px",
+      textAlign: "left"
+    },
+    panelDetailsSectionText: {
+      color: '#1C2B39',
+      fontWeight: '300',
+      fontSize: '16px',
+      lineHeight: '14px',
+      margin: "0px",
+      textAlign: "left",
+    },
+    panelActions: {
+      padding: '7px',
+      borderTop: '1px solid rgba(96,106,116,0.4)',
+    },
+    actions: {
+      textAlign: 'right',
+      paddingTop: '24px',
+    },
+  });
+
+
+
+export default withStyles(styles)(ReviewList);
+
+
+{/* <tr key={enzRow.index}>
+<td >{enzRow.index}</td> 
+<td>{enzRow.firstName}</td> 
+<td>{enzRow.lastName}</td>  
+<td>{enzRow.occupation}</td>  
+<td>{enzRow.ethnicity}</td>      
+<td>{enzRow.gender}</td>      
+<td>{enzRow.agencyRep}</td>   
+<td>{enzRow.bornTown}</td>  
+<td>{enzRow.bornState}</td>
+<td>{enzRow.education}</td>
+<td>{enzRow.playsWith}</td>
+<td>{enzRow.wikiPage}</td>                       
+</tr> */}
--- client/src/scenes/Energizers/components/Upload/index.jsx
+++ client/src/scenes/Energizers/components/Upload/index.jsx
@@ -9,6 +9,7 @@ import DialogActions from '@material-ui/core/DialogActions';
 import Button from '@material-ui/core/Button';
 import * as api from '../../../../services/api';
 import XLSX from 'xlsx';
+import ReviewList from './ReviewList';
 
 const styles = () => ({
   input: {
@@ -41,17 +42,46 @@ class UploadPage extends Component {
   constructor(props) {
     super(props);
 
+    let columnMap = []
+    columnMap[0]='index' 
+    columnMap[1]='firstName'
+    columnMap[2]='lastName'
+    columnMap[3]='occupation'
+    columnMap[4]='ethnicity'
+    columnMap[5]='gender'
+    columnMap[6]='agencyRep'
+    columnMap[7]='bornTown'
+    columnMap[8]='bornState'
+    columnMap[12]='education'
+    columnMap[15]='playsWith'
+    columnMap[17]='wikiPage'
+    //=====not included===== 
+    columnMap[9]='zipCode'
+    columnMap[10]='bornCountry'
+    columnMap[11]='highSchool'
+    columnMap[13]='instagram'
+    columnMap[14]='twitter'
+    columnMap[16]='imdb'
+
+    let usedColumnMap = []
+    let usedColumns = [0,1,2,3,4,5,6,7,8,12,15,17]
+    
+    columnMap.forEach((header, index) => {
+        if (usedColumns.includes(index)) usedColumnMap.push(header)
+    })
+
+
     this.state = {
         selectedFile: "", 
-        fileName: " - ? - ",
-        listData: null,
-        openReviewUploadResultsModal:false
+        fileName: "",
+        cleanUploadList: [],
+        openReviewListModal:false,
+        columnMap, 
+        usedColumnMap
     };
   }
 
-  processFile = async () => {
-
-
+  readFile = async () => {
         var reader = new FileReader();
         reader.onload = async (file) =>  { //needs to be arrow function
             try {
@@ -59,11 +89,13 @@ class UploadPage extends Component {
                         let readData = XLSX.read(data, {type: 'binary'});
                         const wsname = readData.SheetNames[0];
                         const ws = readData.Sheets[wsname];
-                        let listData= XLSX.utils.sheet_to_json(ws, {header:1});
-                        console.log("JSONData", listData) 
+                        let rawJSON= XLSX.utils.sheet_to_json(ws, {header:1});
+                        //console.log("JSONData", rawJSON) 
+                        let cleanUploadList = this.processJSON(rawJSON)
+
                         this.setState({
-                            listData,
-                            openReviewUploadResultsModal: true
+                            cleanUploadList,
+                            openReviewListModal: true
                         });
                         this.props.enqueueSnackbar('List Read!');
             } catch {
@@ -72,32 +104,47 @@ class UploadPage extends Component {
             )}
 
         };
-        reader.readAsBinaryString(this.state.selectedFile)
-    
-        
+        reader.readAsBinaryString(this.state.selectedFile) 
+  }; 
 
-
+  processJSON = (rawJSON) => {
  
-  }; //readFile
+        let columnMap = this.state.columnMap
+        let results = []
+
+        rawJSON.forEach ((row,index) => {
+            let newEnergizer={}    
+            newEnergizer["index"] = index
+            for (let col =0;col<row.length;col++) {
+                newEnergizer[columnMap[col+1]] = row[col]
+            }
+           //console.log(newEnergizer)
+           results.push(newEnergizer)
+        })
+        results.shift()//remove headers
+        return results
+        
+  }
 
 
   onOpenFileSelector = (event) => {
     event.preventDefault();
     let fileObj = event.target.files[0];
-    console.log("IN Open File Selector, selected: ", fileObj.name) 
-
-
+    
     this.setState({
         selectedFile:fileObj, 
         fileName: event.target.files[0].name
     })
-
   }
 
+sendList = () => {
+    console.log("Sending List")
+}
 
 
   render() {
-    const {  onClose } = this.props;
+    const {  onClose, sendList } = this.props;
+    const {  cleanUploadList, columnMap, usedColumnMap, openReviewListModal } = this.state;
 
     return (
       <Dialog open fullWidth onClose={ onClose } maxWidth={ 'lg' }>
@@ -119,12 +166,25 @@ class UploadPage extends Component {
           <DialogContent>
 
 
+          {openReviewListModal && (
+            <div>
+              <ReviewList
+                onClose={onClose}
+                rowsList={cleanUploadList}
+                sendList={sendList}
+                columnMap = {usedColumnMap}
+
+              />
+            </div>
+            )}
+
+
           </DialogContent>
 
           <DialogActions>
         
               <Button color="primary" variant="contained" onClick={onClose}>Cancel</Button>
-              <Button color="primary" variant="contained" onClick={this.processFile}>Process {this.state.fileName}</Button>
+              <Button color="primary" variant="contained" onClick={this.readFile}>Process {this.state.fileName}</Button>
           </DialogActions>
       </Dialog>
     );
